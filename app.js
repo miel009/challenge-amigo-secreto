76cb8bb44d600d1cd84e02b913ea4095462f41cb
NineTravel <melanyascencio908@gmail.com>
2025-09-12 21:12:07 -0300

    feat: sortear amigo

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 //permite agregar nombres (validacion de ingreso de datos)
 let amigos = [];
-function agregarAmigo() { 
-    let input = document.querySelector("#amigoNombre").value; 
+
+function agregarAmigo() {
+    let input = document.querySelector("#amigoNombre").value;
     // validacion
-    if (input.trim() === "") { 
+    if (input.trim() === "") {
         alert("ingrese un nombre valido..");
-    } else if(!input.trim() > 5) {
+    } else if (amigos.length >= 5) {
         alert('no puede ingresar mas amigos');
-    }else{
-        // logica para agregar al amigos - array
+    } else {
         amigos.push(input); // agrega al array los nombres ingresados por el usuario
-        let mostrarlista = document.querySelector('ul');
-        mostrarlista.innerHTML =  'Lista de amigos:' + ('<br>') + '<ul>' + amigos.map(amigo => `<li>${amigo}</li>`).join('') + '</ul>';
+        let mostrarlista = document.getElementById('listaAmigos');
+        mostrarlista.innerHTML = " Lista de amigos:" + ('<br>') + amigos.map(amigo => `<li>${amigo}</li>`).join('');
         limpiarInput();
     }
 
 }
-function limpiarInput(){
-    let limpiar= document.querySelector('#amigoNombre');
-    limpiar.value='';
+function limpiarInput() {
+    let limpiar = document.querySelector('#amigoNombre');
+    limpiar.value = '';
 }
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        return null;
+    } else {
+        let amixElegido = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[amixElegido];
+        document.getElementById('resultado').innerHTML = `<li>${amigoSorteado}</li>`;
+        return amigoSorteado;
+    }
+}
+
 
